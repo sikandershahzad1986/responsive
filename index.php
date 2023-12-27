724335ae9310bbf1e68dd4d0581c819bf120f9dd
sikandershahzad1986 <154625491+sikandershahzad1986@users.noreply.github.com>
2023-12-27 21:57:28 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Responsive Design</title>
-
-
 	<style>
 		body{
 			background-color: pink;
@@ -12,7 +10,6 @@
 
 		@media (min-width: 375px) and (max-width: 667px)
 		{
-
 			body{
 				background-color: seagreen;
 			}
@@ -20,7 +17,6 @@
 
 		@media (min-width: 390px) and (max-width: 884px)
 		{
-
 			body{
 				background-color: teal;
 			}
@@ -28,18 +24,12 @@
 
 		@media (min-width: 1024px) and (max-width: 1366px)
 		{
-
 			body{
 				background-color: tomato;
 			}
 		}
-	
-
 	</style>
 </head>
 <body>
-
-
-
 </body>
 </html>
