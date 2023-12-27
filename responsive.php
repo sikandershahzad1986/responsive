36ba4622877661b1c973fff9185717dcf3c84b62
sikandershahzad1986 <154625491+sikandershahzad1986@users.noreply.github.com>
2023-12-27 21:45:52 +0500

    Add files via upload

---
+++ responsive.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Responsive Design</title>
+
+
+	<style>
+		body{
+			background-color: pink;
+		}
+
+		@media (min-width: 375px) and (max-width: 667px)
+		{
+
+			body{
+				background-color: seagreen;
+			}
+		}
+
+		@media (min-width: 390px) and (max-width: 884px)
+		{
+
+			body{
+				background-color: teal;
+			}
+		}
+
+		@media (min-width: 1024px) and (max-width: 1366px)
+		{
+
+			body{
+				background-color: tomato;
+			}
+		}
+	
+
+	</style>
+</head>
+<body>
+
+
+
+</body>
+</html>
